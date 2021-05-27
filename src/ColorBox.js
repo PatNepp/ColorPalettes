a5ecd96b8611e26057994f67b2e4e8273e5e0129
Patrick Nepp <ptnepp@gmail.com>
2021-05-26 22:18:19 -0500

    copied to clipboard

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
 import './ColorBox.css';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 class ColorBox extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { copied: false };
+	}
+	changeCopyState = () => {
+		this.setState({ copied: true }, () => {
+			setTimeout(() => this.setState({ copied: false }), 1500);
+		});
+	};
 	render() {
+		const { copied } = this.state;
 		const { name, background } = this.props;
 		return (
-			<div style={{ background: background }} className="ColorBox">
-				<div className="copy-container">
-					<div className="box-content">
-						<span>{name}</span>
+			<CopyToClipboard text={background} onCopy={this.changeCopyState}>
+				<div style={{ background }} className="ColorBox">
+					<div style={{ background }} className={`copy-overlay ${copied && 'show'}`} />
+					<div className={`copy-msg ${copied && 'show'}`}>
+						<h1>copied!</h1>
+						<p>{background}</p>
 					</div>
-					<button className="copy-button">copy</button>
+					<div className="copy-container">
+						<div className="box-content">
+							<span>{name}</span>
+						</div>
+						<button className="copy-button">copy</button>
+					</div>
+					<span className="see-more">more</span>
 				</div>
-				<span className="see-more">more</span>
-			</div>
+			</CopyToClipboard>
 		);
 	}
 }
